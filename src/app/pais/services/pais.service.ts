b80a1392b22ca79bd9be26ed8f7a0372e14fd71c
Cotze <carlosrodigoatl@gmail.com>
2022-01-25 13:02:06 -0600

    Optimisacion de las peticiones HTTP

--- src/app/pais/services/pais.service.ts
+++ src/app/pais/services/pais.service.ts
@@ -1,7 +1,8 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable, of } from 'rxjs';
 import { Country } from '../interfaces/pais.interface';
+import { tap } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
@@ -10,11 +11,16 @@ export class PaisService {
 
   private apiUrl: string = 'https://restcountries.com/v2'
 
+  get httpParams(){
+    return new HttpParams()
+    .set( 'fields', 'name,capital,alpha2Code,flag,population')
+  }
+
   constructor( private http: HttpClient) { }
 
   buscarPais( termino: string): Observable<Country[]> {
     const url = `${ this.apiUrl }/name/${ termino }`
-    return this.http.get<Country[]>( url );
+    return this.http.get<Country[]>( url, { params: this.httpParams} );
             //puede ser bastante util para manejar u obtener los errores
   //           .pipe(
   //             catchError( arr => of([]) )
@@ -23,7 +29,7 @@ export class PaisService {
 
   buscarCapital( termino: string ): Observable<Country[]>{
     const url = `${ this.apiUrl }/capital/${ termino }`
-    return this.http.get<Country[]>( url );
+    return this.http.get<Country[]>( url, {params: this.httpParams} );
   }
 
   getPaisPorId( id: string ): Observable<Country>{
@@ -32,8 +38,13 @@ export class PaisService {
   }
 
   buscarRegion(region: string ): Observable<Country[]>{
-    const url = `${this.apiUrl}/regionalbloc/${region}`
-    return this.http.get<Country[]>(url);
+
+    const url = `${this.apiUrl}/regionalbloc/${region}`;
+    return this.http.get<Country[]>(url, { params: this.httpParams })
+        .pipe(
+          tap(console.log);
+          )
+        )
   }
   
 }
